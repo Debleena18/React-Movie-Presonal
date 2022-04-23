01e7c6f4ed5506afde3365407ab5c8070a01c7ea
Debleena18 <sarkardebleena18@gmail.com>
2022-04-23 16:51:58 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 //import Home from "../src/pages/home/Home";
 import './App.scss';
+import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 //import Watch from "./pages/Watch/Watch";
 
 function App() {
   return (
     <div className="style">
+      {/* <Home /> */}
       {/* <Watch /> */}
-      <Register />
+      {/* <Register /> */}
+      <Login />
       </div>
   );
 }
