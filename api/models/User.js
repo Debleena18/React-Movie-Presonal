0a1f781335b303d10ad2a278b6448b33330fa5d0
Debleena18 <sarkardebleena18@gmail.com>
2022-04-24 15:18:59 +0530

    api models

---
+++ api/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+//Schema creation.
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profilePic: { type: String, defaut: "" },
+    isAdmin: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
+//Modal name: User and Ref point: UserSchema
